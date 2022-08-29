f82ed5b288687b98c9b8871deedabb4bd76af760
Lê Văn Hiếu <104577452+levanhieu652002@users.noreply.github.com>
2022-08-29 10:57:53 +0700

    Final Layout

---
+++ GiaoDien_2.java
@@ -0,0 +1,14 @@
+package com.example.giaodien_117;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GiaoDien_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_giao_dien2);
+    }
+}
